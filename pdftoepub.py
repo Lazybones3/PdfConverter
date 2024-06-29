18f5db542809d4149597813a91dd1b3d5a0e0df4
Lazybones3 <caorui452@163.com>
2024-06-29 17:10:51 +0800

    pdftoepub

--- pdftoepub.py
+++ pdftoepub.py
@@ -2,90 +2,44 @@ import sys
 import os
 import pymupdf
 from ebooklib import epub
-from typing import List
 
 
-class PdfChapter:
-    def __init__(self, id: int, title: str, content: str, page: int) -> None:
-        self.id = id
-        self.title = title
-        self.content = content
-        self.page = page
-
-
-class PdfDocument:
-    def __init__(self, title: str, author: str, chapter: List = []) -> None:
-        self.title = title
-        self.author = author
-        self.chapters = chapter
-
-
-class PdfConverter:
-    def __init__(self, filename: str) -> None:
-        self.filename = filename
-        self.pdf_doc = None
-
-    def read_pdf(self) -> PdfDocument:
-        doc = pymupdf.open(self.filename)
-        toc = doc.get_toc()
-        chapter_list = []
-        # [lvl, title, page]
-        for i in range(len(toc) - 1):
-            _, title, cur_page = toc[i]
-            _, _, next_page = toc[i + 1]
-            content = ''
-            if cur_page == next_page:
-                continue
-            for page in doc.pages(cur_page-1, next_page-1):
-                content += page.get_text("xhtml")
-            chapter = PdfChapter(i+1, title, content, cur_page)
-            chapter_list.append(chapter)
-        title = doc.metadata["title"]
-        author = doc.metadata["author"]
-        if title is None or title == '':
-            title, _ = os.path.splitext(os.path.basename(self.filename))
-        self.pdf_doc = PdfDocument(title, author, chapter_list)
-
-
-    def write_epub(self) -> None:
-        book = epub.EpubBook()
-        document = self.pdf_doc
-
-        # set metadata
-        book.set_title(document.title)
-        book.set_language("en")
-
-        book.add_author(document.author)
-
-        # basic spine
-        book.spine = ['nav']
-
-        # create chapter
-        for c in document.chapters:
-            chapter_id = f"chap_{c.id}"
-            file_name = f"{chapter_id}.xhtml"
-            chapter = epub.EpubHtml(uid=chapter_id, title=c.title, file_name=file_name, lang="en")
-            chapter.content = c.content
-
-            # add chapter
-            book.add_item(chapter)
-
-            # define Table Of Contents
-            book.toc.append(epub.Link(file_name, c.title, chapter_id))
-
-            book.spine.append(chapter_id)
-
-        # add default NCX and Nav file
-        book.add_item(epub.EpubNcx())
-        book.add_item(epub.EpubNav())
-
-        # write to the file
-        output_file = self.filename.replace(".pdf", ".epub")
-        epub.write_epub(output_file, book)
-
-    def pdf_to_epub(self):
-        self.read_pdf()
-        self.write_epub()
+def convert_pdf_to_epub(filename):
+    # Read from Pdf
+    doc = pymupdf.open(filename)
+    # Write to Epub
+    book = epub.EpubBook()
+    # set metadata
+    book.set_language("en")
+    title = doc.metadata["title"]
+    if title is None or title == '':
+        title, _ = os.path.splitext(os.path.basename(filename))
+    book.set_title(title)
+    author = doc.metadata["author"]
+    book.add_author(author)
+    # basic spine
+    book.spine = ['nav']
+    for i, page in enumerate(doc.pages()):
+        page_id = f"page_{i + 1}"
+        file_name = f"{page_id}.xhtml"
+        page_html = epub.EpubHtml(uid=page_id, title=page_id, file_name=file_name, lang="en")
+        page_html.content = page.get_text("xhtml")
+        # add chapter
+        book.add_item(page_html)
+        book.spine.append(page_id)
+    # define Table Of Contents
+    # [lvl, title, page]
+    for toc in doc.get_toc():
+        page_id = f"page_{toc[2]}"
+        file_name = f"{page_id}.xhtml"
+        book.toc.append(epub.Link(file_name, toc[1], page_id))
+    # add default NCX and Nav file
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+    # write to the file
+    output_file = filename.replace(".pdf", ".epub")
+    epub.write_epub(output_file, book)
+    doc.close()
 
 
 if __name__ == '__main__':
@@ -93,5 +47,4 @@ if __name__ == '__main__':
         print("Usage: pdftoepub <filename>")
         sys.exit()
     filename = sys.argv[1]
-    converter = PdfConverter(filename)
-    converter.pdf_to_epub()
+    convert_pdf_to_epub(filename)
